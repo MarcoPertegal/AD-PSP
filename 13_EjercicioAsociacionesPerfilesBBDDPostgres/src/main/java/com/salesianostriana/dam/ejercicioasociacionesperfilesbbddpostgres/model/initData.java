67e3ad47d4a7947c3817e898f81555c50479c86f
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2023-11-03 14:32:34 +0100

    a

---
+++ 13_EjercicioAsociacionesPerfilesBBDDPostgres/src/main/java/com/salesianostriana/dam/ejercicioasociacionesperfilesbbddpostgres/model/initData.java
@@ -0,0 +1,18 @@
+package com.salesianostriana.dam.ejercicioasociacionesperfilesbbddpostgres.model;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class initData {
+
+    @PostConstruct
+    public void init(){
+        Profesor p1 = Profesor.builder()
+                .nombre("Juan")
+                .email("juan@gmail.com")
+                .build();
+    }
+}
--- 13_EjercicioAsociacionesPerfilesBBDDPostgres/src/main/java/com/salesianostriana/dam/ejercicioasociacionesperfilesbbddpostgres/repository/VideoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salesianostriana.dam.ejercicioasociacionesperfilesbbddpostgres.repository;
-
-import com.salesianostriana.dam.ejercicioasociacionesperfilesbbddpostgres.model.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, Long> {
-}
