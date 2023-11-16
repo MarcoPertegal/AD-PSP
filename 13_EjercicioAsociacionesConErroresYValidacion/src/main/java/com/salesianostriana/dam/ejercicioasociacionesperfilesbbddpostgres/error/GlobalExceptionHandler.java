dc698c416b6183b48378bf3c82a36ca6514cd022
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2023-11-16 12:38:05 +0100

    Algunas correcciones

--- 13_EjercicioAsociacionesConErroresYValidacion/src/main/java/com/salesianostriana/dam/ejercicioasociacionesperfilesbbddpostgres/error/GlobalExceptionHandler.java
+++ 13_EjercicioAsociacionesConErroresYValidacion/src/main/java/com/salesianostriana/dam/ejercicioasociacionesperfilesbbddpostgres/error/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.salesianostriana.dam.ejercicioasociacionesperfilesbbddpostgres.error;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
@@ -19,11 +23,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return problemDetail;
     }
 
+    //para validaxion
     @ExceptionHandler(ProfesorNotFoundException.class)
-    ProblemDetail handleNombreNullException(ProfesorNombreNullException p){
+    ProblemDetail handleMethodArgumentNotValid(ProfesorNombreNullException p){
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_ACCEPTABLE, p.getMessage());
         problemDetail.setTitle("Profesor name cant be null");
         problemDetail.setType(URI.create("https://api.ejercicioprueba.com/errors/null"));
         return problemDetail;
     }
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return buildApiErrorWithSubErrors("Validation error. Please check the sublist.", request, status, ex.getAllErrors());
+    }
+
 }
