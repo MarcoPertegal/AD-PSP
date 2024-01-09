8c98c06f671e3cbaebd61f97d8a3fd066da8500d
Marco <marco.pertegal.prieto@gmail.com>
2024-01-09 19:37:18 +0100

    Example tests based on HashSet removeAll() method

--- Tests/pruebaJUnit/src/test/java/org/example/AppTest.java
+++ Tests/pruebaJUnit/src/test/java/org/example/AppTest.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
@@ -17,7 +17,8 @@ public class AppTest {
     }
 
     //los metodos asset comprueban que se cumple algo, es una especie de comprobacion if
-
+    //TEST Basados en la tabla de los apuntes
+    //con removeAll se eliminan todos los elementos de la lista que coincidan con la lista c
     @Test
     void test1(){
 
@@ -25,7 +26,65 @@ public class AppTest {
         Collection<Integer> c = Collections.emptyList();
 
         assertFalse(hs.removeAll(c));
-        //assertEquals()
-
+        assertEquals(5, hs.size());
     }
+
+    @Test
+    void test2(){
+
+        HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4,5));
+        List<Integer> c = List.of(1);
+
+        assertTrue(hs.removeAll(c));
+        assertEquals(4, hs.size());
+    }
+
+    @Test
+    void test3(){
+
+        HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4,5));
+        List<Integer> c = List.of(1,2,3,4,5,6);
+
+        assertTrue(hs.removeAll(c));
+        assertEquals(0, hs.size());
+    }
+
+    @Test
+    void test4(){
+
+        HashSet<Integer> hs = new HashSet<>();
+        List<Integer> c = List.of(1,2,3,4,5,6);
+
+        assertFalse(hs.removeAll(c));
+        assertEquals(0, hs.size());
+    }
+
+    @Test
+    void test5(){
+
+        HashSet<Integer> hs = new HashSet<>(List.of(7,8));
+        List<Integer> c = List.of(1,2,3,4,5,6);
+
+        assertFalse(hs.removeAll(c));
+        assertEquals(2, hs.size());
+    }
+
+    @Test
+    void test7(){
+
+        HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4,5));
+        List<Integer> c = List.of(6);
+
+        assertFalse(hs.removeAll(c));
+        assertEquals(5, hs.size());
+    }
+    /*
+    @Test
+    void nullPointer(){
+
+        HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4,5));
+        List<Integer> c = List.of(null);
+
+        assertFalse(hs.removeAll(c));
+    }*/
 }
